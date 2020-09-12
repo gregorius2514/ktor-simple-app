85070a392e2f7b4977cfca16627c9e688fa867dd
Grzesiek <grzesiek-kielce@o2.pl>
2020-09-12 23:14:35 +0200

    Change interface for AppRoutes modules to more generic

--- deployment/src/main/kotlin/com/codefun/workshop/AppRoutes.kt
+++ deployment/src/main/kotlin/com/codefun/workshop/AppRoutes.kt
@@ -1,11 +1,13 @@
 package com.codefun.workshop
 
-import com.google.inject.AbstractModule
+import com.google.inject.Binder
+import com.google.inject.Module
 
-class AppRoutes : AbstractModule() {
-    override fun configure() {
-        bind(HelloRoutes::class.java).asEagerSingleton()
-        bind(UserRoutes::class.java).asEagerSingleton()
+class AppRoutes : Module {
+
+    override fun configure(binder: Binder) {
+        binder.bind(HelloRoutes::class.java).asEagerSingleton()
+        binder.bind(UserRoutes::class.java).asEagerSingleton()
     }
 }
 
